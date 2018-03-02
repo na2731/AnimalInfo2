9cdff528c13563c0b1d0aba2355abeb770f5fc1f
na2731 <na2731@tc.columbia.edu>
2018-03-01 23:12:30 -0500

    update

--- app/src/main/java/com/example/android/animalinfo/AnimalInfo.java
+++ app/src/main/java/com/example/android/animalinfo/AnimalInfo.java
@@ -17,32 +17,18 @@ public class AnimalInfo extends AppCompatActivity {
         Animal a = (Animal) i.getSerializableExtra("MyClass");
 
 
-
-        //Intent intent = getIntent().getSerializableExtra("MyClass");;
-
-
-//        String temp = a.getDescription().replace("\\n", "n");
-//
-//        Animal.setDescription(temp);
-//        i.putExtra(Keys.ANINAME);
-
-
         display(a);
     }
 
-     public void display(Animal a){
+    public void display(Animal a) {
         TextView ReSult = (TextView) findViewById(R.id.result);
-//        if(a.getAttribute1() == false && a.getAttribute2() == false){
-//            ReSult.setText("Name:" + a.getName() + '\n' + "Number of Legs:" + a.getLegs()+'\n' +"Does it have Fur?" + a.getFur()+'\n'+"Does it Carnivores?"+ a.getAttribute1()+'\n'+"Any Additional Information:"+ a.getInfo());
-//        }
-        if(a.getAttribute1() == true && a.getAttribute2() == false){
-            ReSult.setText(Keys.ANINAME + a.getName() + '\n' + Keys.NUM + a.getLegs()+'\n' + Keys.FUR + a.getFur()+'\n'+ Keys.ATTRIBUTE + "Carnivores" +'\n'+ Keys.ADD + a.getInfo());
-        }
-        else if(a.getAttribute1() == false && a.getAttribute2() == true){
-             ReSult.setText(Keys.ANINAME + a.getName() + '\n' + Keys.NUM  + a.getLegs()+'\n' + Keys.FUR + a.getFur()+'\n'+ Keys.ATTRIBUTE + "Herbivores" +'\n'+ Keys.ADD + a.getInfo());
-        }
-        else{
-            ReSult.setText(Keys.ANINAME + a.getName() + '\n' + Keys.NUM  + a.getLegs()+'\n' + Keys.FUR + a.getFur()+'\n'+ Keys.ATTRIBUTE + " You don't eat??" +'\n'+ Keys.ADD + a.getInfo());
+
+        if (a.getAttribute1() && !a.getAttribute2()) {
+            ReSult.setText(Keys.ANINAME + a.getName() + '\n' + Keys.NUM + a.getLegs() + '\n' + Keys.FUR + a.getFur() + '\n' + Keys.ATTRIBUTE + "Carnivores" + '\n' + Keys.ADD + a.getInfo());
+        } else if (!a.getAttribute1() && a.getAttribute2()) {
+            ReSult.setText(Keys.ANINAME + a.getName() + '\n' + Keys.NUM + a.getLegs() + '\n' + Keys.FUR + a.getFur() + '\n' + Keys.ATTRIBUTE + "Herbivores" + '\n' + Keys.ADD + a.getInfo());
+        } else {
+            ReSult.setText(Keys.ANINAME + a.getName() + '\n' + Keys.NUM + a.getLegs() + '\n' + Keys.FUR + a.getFur() + '\n' + Keys.ATTRIBUTE + " You don't eat??" + '\n' + Keys.ADD + a.getInfo());
         }
     }
 
--- app/src/main/java/com/example/android/animalinfo/MainActivity.java
+++ app/src/main/java/com/example/android/animalinfo/MainActivity.java
@@ -18,33 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        Intent intent = getIntent();
-//        Animal a = (Animal)intent.getSerializableExtra(Keys);
     }
 
-//    public void onRadioButtonClicked(View view) {
-//        // Is the button now checked?
-//        boolean checked = ((RadioButton) view).isChecked();
-//
-//        // Check which radio button was clicked
-//        switch(view.getId()) {
-//            case R.id.one:
-//                if (checked)
-//                    // Pirates are the best
-//                    break;
-//            case R.id.two:
-//                if (checked)
-//                    // Ninjas rule
-//                    break;
-//            case R.id.three:
-//                if(checked)
-//                    break;
-//        }
-//    }
 
     public void openSecActivity(View view){
-//        System.out.print("enter");
         Intent i=new Intent(this,AnimalInfo.class);
 
         //i.putExtra(Keys.ANINAME, "animal name: ");
@@ -80,72 +57,7 @@ public class MainActivity extends AppCompatActivity {
         i.putExtra("MyClass", a);
 
 
-
-
-//        Animal a = new Animal(name, num, hascheckBox, info);
-//        i.putExtra(Keys.ANIMAL)
-        //i.putExtra(Keys., a);
-
-
-
-
-
-//        EditText edit = (EditText)findViewById(R.id.aniName);
-//        String name = edit.getText().toString();
-//        i.putExtra(Keys.ANINAME, name);
-//        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
-//        boolean hascheckBox = checkBox.isChecked();
-//        i.putExtra(Keys.FUR, hascheckBox);
-
-//        EditText edit2 = (EditText)findViewById(R.id.legs);
-//        int num = Integer.parseInt(edit2.getText().toString());
-//        i.putExtra(Keys.NUM, num);
-
-//        EditText edit3 = (EditText)findViewById(R.id.moreInfo);
-//        String info = edit3.getText().toString();
-//        i.putExtra(Keys.ADD, info);
-
-
-
         startActivity(i);
     }
 
-//    public void AnotherActivity(View view) {
-//        Intent i=new Intent(this,AnimalInfo.class);
-//        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
-//        boolean hascheckBox = checkBox.isChecked();
-//
-//        if (!hascheckBox) {
-//            CheckBox box = (CheckBox)findViewById(R.id.checkBox);
-//            boolean value = box.isChecked();
-//            i.putExtra(Keys.FUR, value);
-//        }
-//    }
-
-//    public class Animal implements Serializable {
-//        private String name;
-//        private int legs;
-//        private boolean Fur;
-//        private String info;
-//
-//        public Animal(String name, int legs, boolean Fur, String info){
-//            this.name=name;
-//            this.legs=legs;
-//            this.Fur=Fur;
-//            this.info=info;
-//        }
-//
-//
-//        public String getName(){
-//            return name;
-//        }
-//
-//        public int getLegs(){
-//            return legs;
-//        }
-//
-//        public boolean getFur(){
-//            return Fur;
-//        }
-//    }
 }
